3762b3ef2729ed617ffc2847403e46c8f51b883a
Deadstage <scottgarethmansley@gmail.com>
2023-09-07 15:51:38 +0100

    Starting Menu
    Commit before starting work on a main menu and save function.

--- Assets/Scripts/RoomManager.cs
+++ Assets/Scripts/RoomManager.cs
@@ -8,21 +8,32 @@ public class RoomManager : MonoBehaviour
     public GameObject virtualCam;
     public GameObject CMvcam1;
 
-    private EnemyOriginalPosition[] enemyPositions; // Define an array of EnemyOriginalPosition objects
+    private EnemyOriginalPosition[] enemyPositions; // Array of EnemyOriginalPosition objects
+    private PolygonCollider2D boundary; // reference to the boundary of this room
 
     private void Start()
     {
         CMvcam1 = GameObject.FindGameObjectWithTag("firstCam");
         CMvcam1.SetActive(true);
+        boundary = GetComponent<PolygonCollider2D>(); // get the boundary at start
 
-        GameObject[] enemies = GameObject.FindGameObjectsWithTag("Enemy");
+        GameObject[] allEnemies = GameObject.FindGameObjectsWithTag("Enemy"); // Get all enemies in the scene
 
-        // Populate the enemyPositions array with the EnemyOriginalPosition components of the enemies
-        enemyPositions = new EnemyOriginalPosition[enemies.Length];
-        for (int i = 0; i < enemies.Length; i++)
+        // Filter the enemies that are in this room
+        List<EnemyOriginalPosition> enemiesInRoom = new List<EnemyOriginalPosition>();
+        foreach (var enemy in allEnemies)
         {
-            enemyPositions[i] = enemies[i].GetComponent<EnemyOriginalPosition>();
-        }     
+            if (boundary.OverlapPoint(enemy.transform.position)) // if the enemy is in this room
+            {
+                var enemyPos = enemy.GetComponent<EnemyOriginalPosition>();
+                enemiesInRoom.Add(enemyPos);
+
+                // Deactivate the enemy initially
+                enemyPos.gameObject.SetActive(false);
+            }
+        }
+
+        enemyPositions = enemiesInRoom.ToArray(); // Convert list to array
     }
 
     private void OnTriggerEnter2D(Collider2D other)
@@ -31,11 +42,11 @@ public class RoomManager : MonoBehaviour
         {
             virtualCam.SetActive(true);
 
-            // Loop through the enemyPositions array and call the EnemyRespawn() method for each enemy
+            // Activate and reset all the enemies in the room
             foreach(var enemyPos in enemyPositions)
             {
-                enemyPos.respawnTrigger = true;
-                enemyPos.EnemyRespawn();
+                enemyPos.gameObject.SetActive(true);
+                enemyPos.EnemyRespawn(); // reset the enemy to its original state
             }
         }
     }
@@ -46,17 +57,11 @@ public class RoomManager : MonoBehaviour
         {
             virtualCam.SetActive(false);
 
+            // Deactivate all the enemies in the room
             foreach(var enemyPos in enemyPositions)
             {
-
-                enemyPos.respawnTrigger = false;
-
+                enemyPos.gameObject.SetActive(false);
             }
-            // enemyOriginalPosition[1].respawnTrigger = false;
-            // enemyOriginalPosition[2].respawnTrigger = false;
-            // enemyOriginalPosition[3].respawnTrigger = false;
-            //Debug.Log(enemyOriginalPosition.respawnTrigger);
-
         }
     }
 
